8d4898566e2d828af01e03521eabe32cb9db34cd
DAK <46497387+DAK404@users.noreply.github.com>
2024-01-24 23:10:34 +0530

    Start adding essential features
    ****** NOTE ******
    
    WORK IN PROGRESS!
    
    Code cleanup, documentation and feature implementation needs to be done.
    
    *******************
    
    * Added the following classes: Anvil, FileDownload, FileManagement, FileRead, FileWrite and NionUpdate
    
    NOTE: The following classes are the renamed classes for readability sake.
    
    Download -> FileDownload
    WraithRead -> FileRead
    WraithEdit -> FileWrite
    Updater -> NionUpdate
    
    NionUpdate shall contain the Installer.java logic too.

--- Source/Cataphract/API/Anvil.java
+++ Source/Cataphract/API/Anvil.java
@@ -6,7 +6,7 @@ package Cataphract.API;
 //ANY EXTRA FEATURES ARE PASSED BACK TO THE PROGRAM AND ARE PROCESSED BY THE CALLING PROGRAM
 
 /**
- *
+ * 
  */
 public class Anvil
 {
@@ -15,28 +15,36 @@ public class Anvil
     */
     public Anvil()
     {
-
     }
 
     /**
      * Module implementing the interpreter for common commands. Also helps in scripting and reduces code duplication.
      *
-     * @param command
-     * @return
+     * @param command : Command String that the interpreter shall... Interpret
+     * @return boolean status: Returns true if the command entered is found, else returns false
      * @throws Exception : Throws any exceptions encountered during runtime.
      */
     public static boolean anvilInterpreter(String command)throws Exception
     {
+        //Set the return value to be true, false is returned when none of the commands are found.
         boolean status = true;
+
+        //Split the string and store them into an array for processing
         String[] commandArray = splitStringToArray(command);
 
+        //Logic to check which command was fed into the interpreter. Converted to lowercase for avoiding case sensitivity
         switch(commandArray[0].toLowerCase())
         {
+            //Clear: Clears the screen, calls the viewBuildInfo() to display the build info and clear the rest of the contents
+            //TO-DO: add a "force" option to not display even the branding
             case "clear":
                 Build.viewBuildInfo();
             break;
 
+            //Echo: Prints a string on the display
+            //TO-DO: Print options for error, warning, attention and information.
             case "echo":
+                //Display an error message if the entered syntax is incorrect
                 if(commandArray.length < 2)
                     IOStreams.println("echo <STRING> \n\nOR\n\necho \"<STRING_WITH_SPACES>\"");
                 else
@@ -57,11 +65,13 @@ public class Anvil
             //     Build.aboutProgram();
             // break;
 
+            //Wait: Waits for the specified value (milliseconds) for the shell to wait for a second
             case "wait":
                 try
                 {
+                    //Display an error message if the entered syntax is incorrect
                     if(Integer.parseInt(commandArray[1]) < 1)
-                        IOStreams.println("This will make the prompt wait for a given number of milliseconds.\nSyntax: wait (1000)\n\nPrompt shall wait for 1 second.");
+                        IOStreams.println("This will make the prompt wait for a given number of milliseconds.\nSyntax: wait 1000\n\nPrompt shall wait for 1 second.");
                     else
                         Thread.sleep(Integer.parseInt(commandArray[1]));
                 }
@@ -75,20 +85,27 @@ public class Anvil
                 }
             break;
 
+            //Confirm: requests the user to press RETURN to continue with the execution
             case "confirm":
                 //Check for arguments so that if there are prefixes and suffixes, call the method with the arguments to have a custom string instead of the default.
                 IOStreams.confirmReturnToContinue();
             break;
 
+            //If no command is found, return false. Usually, Sycorax Kernel shall display an error if no commands in both lists are found.
             default:
                 status = false;
             break;
         }
+
+        //Return the status
         return status;
     }
 
     /**
      * Method to split an input string to individual words by at the occurrence of a blank space.
+     * 
+     * @param command : String that will need to be split into an array.
+     * @return String[] : The string split into an array.
      */
     public static String[] splitStringToArray(String command)
     {
---
+++ Source/Cataphract/API/Wyvern/NionUpdate.java
@@ -0,0 +1,23 @@
+package Cataphract.API.Wyvern;
+
+public class NionUpdate
+{
+    public NionUpdate()
+    {
+    }
+
+    public void updater()
+    {
+
+    }
+
+    public void downloadUpdate()
+    {
+
+    }
+
+    public void installUpdate()
+    {
+        
+    }
+}
