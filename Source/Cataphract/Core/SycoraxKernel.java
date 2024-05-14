74276202fcb5ed8fffd49938f53ea65317cfab5d
DAK <46497387+DAK404@users.noreply.github.com>
2024-05-14 07:51:54 +0530

    Begin adding script features
    * Update SycoraxKernel.java to have scripting features (which is in a dormant state)

--- Source/Cataphract/Core/SycoraxKernel.java
+++ Source/Cataphract/Core/SycoraxKernel.java
@@ -1,6 +1,9 @@
 package Cataphract.Core;
 
+import java.io.BufferedReader;
 import java.io.Console;
+import java.io.File;
+import java.io.FileReader;
 
 import Cataphract.API.Anvil;
 import Cataphract.API.Build;
@@ -14,19 +17,23 @@ import Cataphract.API.Dragon.AccountModify;
 import Cataphract.API.Dragon.Login;
 
 import Cataphract.API.Minotaur.Cryptography;
+import Cataphract.API.Minotaur.PolicyCheck;
 
 public class SycoraxKernel
 {
 
-    private String _accountName = "";
-    private String _username = "";
+    private String _accountName = "DEFAULT_USER";
+    private String _username = "DEFAULT_USERNAME";
     private String _userUnlockPIN = "";
+    private String _systemName = "DEFAULT_SYSNAME";
     
     private boolean _isUserAdministrator = false;
+    private boolean _scriptMode = false;
 
     private char _prompt = '?';
 
     private int _loginAttemptsRemaining = 5;
+    private int _scriptLineNumber = 0;
 
     private Console console = System.console();
 
@@ -56,16 +63,20 @@ public class SycoraxKernel
 
     private void userShell() throws Exception
     {
-        String[] commandArray;
         String input = "";
-
         do
         {
-            input = console.readLine(_accountName + "@Cataphract" + _prompt + "> ");
+            input = console.readLine(_accountName + "@" + _systemName + _prompt + "> ");
+            commandProcessor(input);
+        }
+        while(!input.equalsIgnoreCase("logout"));
+    }
 
-            commandArray = Anvil.splitStringToArray(input);
+    private void commandProcessor(String input)throws Exception
+    {
+        String[] commandArray = Anvil.splitStringToArray(input);
 
-            switch(commandArray[0].toLowerCase())
+        switch(commandArray[0].toLowerCase())
             {
                 case "refresh":
                 fetchUserDetails();
@@ -79,9 +90,9 @@ public class SycoraxKernel
                 lockConsole();
                 break;
 
-                case "clear":
-                viewSycoraxInformation();
-                break;
+                // case "clear":
+                // viewSycoraxInformation();
+                // break;
 
                 case "exit":
                 System.exit(0);
@@ -118,8 +129,6 @@ public class SycoraxKernel
                 Anvil.anvilInterpreter(commandArray);
                 break;
             }
-        }
-        while(!input.equalsIgnoreCase("logout"));
     }
 
     private boolean login() throws Exception
@@ -148,6 +157,7 @@ public class SycoraxKernel
         _accountName = new Login(_username).getNameLogic();
         _isUserAdministrator = new Login(_username).checkPrivilegeLogic();
         _userUnlockPIN = new Login(_username).getPINLogic();
+        _systemName = new PolicyCheck().retrievePolicyValue("sysname");
 
         _prompt = _isUserAdministrator?'!':'*';
     }
@@ -187,8 +197,69 @@ public class SycoraxKernel
         return userPIN.equals(_userUnlockPIN);
     }
 
-    private boolean scriptParseLogic(String scriptFileName) throws Exception
+    private boolean anvilScriptEngine(String scriptFileName)throws Exception
     {
-        return false;
+        boolean status = false;
+
+        if(! _isUserAdministrator && ! new Cataphract.API.Minotaur.PolicyCheck().retrievePolicyValue("script").equals("on"))
+        {
+            //to process the anvil script files
+            try
+            {
+                //Check if the name of the script file is a valid string
+                if(scriptFileName == null || scriptFileName.equalsIgnoreCase("") || scriptFileName.startsWith(" "))
+                IOStreams.printError("The name of the script file cannot be be blank.");
+
+                //Check if the script file specified exists.
+                else if(! new File(scriptFileName).exists())
+                //Return an error and pass the control back in case the file is not found.
+                IOStreams.printAttention("Script file "+scriptFileName.replace(_username, _accountName)+" has not been found.\nPlease check the directory of the script file and try again.");
+
+                else
+                {
+                    //Activate the script mode.
+                    _scriptMode = true;
+
+                    //Initialize a stream to read the given file.
+                    BufferedReader br = new BufferedReader(new FileReader(scriptFileName));
+
+                    //Initialize a string to hold the contents of the script file being executed.
+                    String scriptLine = "";
+
+                    //Read the script file, line by line.
+                    while ((scriptLine = br.readLine()) != null)
+                    {
+                        //Check if the line is a comment or is blank in the script file and skip the line.
+                        if(scriptLine.startsWith("#") || scriptLine.equalsIgnoreCase(""))
+                        continue;
+
+                        //Check if End Script command is encountered, which will stop the execution of the script.
+                        else if(scriptLine.equalsIgnoreCase("End Script"))
+                        break;
+
+                        //Read the command in the script file, and pass it on to menuLogic(<command>) for it to be processed.
+                        commandProcessor(scriptLine);
+                        _scriptLineNumber++;
+                    }
+
+                    //Close the streams, run the garbage collector and clean.
+                    br.close();
+                    _scriptLineNumber = 0;
+                    System.gc();
+
+                    //Deactivate the script mode.
+                    _scriptMode = false;
+                }
+            }
+            catch(Exception e)
+            {
+                //handle exception
+            }
+        }
+        else
+        {
+            IOStreams.printError("Insufficient Privileges to run scripts! Please contact the Administrator for more information.");
+        }
+        return status;
     }
 }
